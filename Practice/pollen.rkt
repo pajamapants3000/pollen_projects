621373c2cba2a635ad0e5d871fa9244279e4923d
Tommy Lincoln <pajamapants3000@gmail.com>
2017-05-04 14:37:03 -0700

    ul-w/heading works; section a WIP

--- Practice/pollen.rkt
+++ Practice/pollen.rkt
@@ -25,5 +25,25 @@
   (ol-list ol-items)
   (txexpr 'ol '() (li-items ol-items)))
 
+; Display unordered list with heading
+(define
+  (ul-w/heading items heading)
+  `(list
+    ,(txexpr 'h2 '() `(,heading))
+    "\n"
+    ,(ul-list items)))
+
+; Leverage ul-w/heading for objectives list
+(define
+  (list-objectives objectives)
+  (ul-w/heading objectives "Objectives"))
+
+; WIP: Body of text with variable-level heading
+(define
+  (section d t . content)
+  `(,(string->symbol (format "h~a" d))
+     ,t)
+  `(,content))
+
 #| Other Definitions |#
 
