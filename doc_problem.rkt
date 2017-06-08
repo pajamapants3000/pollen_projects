84a3b0e5a481bff5555b29817d9327403bf8eb11
Tommy Lincoln <pajamapants3000@gmail.com>
2017-06-08 16:16:33 -0700

    doc_problem - level one success!

--- doc_problem.rkt
+++ doc_problem.rkt
@@ -6,15 +6,18 @@
   
   (define (myfunc x . etc)
     (string-join
-      (list
-        (format "'The number is ~a'" (+ 1 x))
-        (string-join etc))))
+     (for/list ([word (flatten etc)])
+       (cond
+         [(mypref? word) (format "'The number is ~a'" (+ (mypref-d word) x))]
+         [else word]))))
 
-(define (myfunc2 . etc)
-  (for/list ([word etc])
-    (cond
-      [(mypref? word) #s(mypref (+ 1 (mypref-d word)))]
-      [else word])))
+ (define (myfunc2 . etc)
+    (list
+     '#s(mypref 1)
+      (for/list ([word (flatten etc)])
+        (cond
+          [(mypref? word) #s(mypref (+ 1 (mypref-d word)))]
+          [else word]))))
                       
 
 (provide myfunc2)
@@ -25,4 +28,4 @@
 
 (require 'mymod)
 
-(display (myfunc2 "let's" (myfunc2 "test") "this"))
+(display (myfunc 3 "let's" (myfunc2 "test") "this"))
