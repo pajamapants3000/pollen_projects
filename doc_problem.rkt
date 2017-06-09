c07d94a0aac71676f42673228d67f0f173f91444
Tommy Lincoln <pajamapants3000@gmail.com>
2017-06-09 11:17:44 -0700

    doc_problem - I GOT IT!

--- doc_problem.rkt
+++ doc_problem.rkt
@@ -7,37 +7,33 @@
   (struct mypref (d t strlist) #:prefab )
   
   (define (chapter . etc)
-     (show-inner etc))
+    (txexpr 'div '()
+            (show-inner etc)))
 
   (define (show-inner inner)
-    (string-join
-     (for/list ([word (flatten inner)])
+    (for/list ([word inner])
+      (cond
+        [(mypref? word) (txexpr 'div '()
+                                (cons
+                                 (txexpr
+                                  (string->symbol (format "h~a" (mypref-d word)))
+                                  '()
+                                  `(,(mypref-t word)))
+                                 (show-inner (mypref-strlist word))))]
+        [else word])))
+
+  (define (section t . etc)
+    (mypref
+     1
+     t
+     (for/list ([word (flatten etc)])
        (cond
-         [(mypref? word) (format "~a\n'The number is ~a'\n~a"
-                                 (mypref-t word)
-                                 (+ (mypref-d word) 0)
-                                 (show-inner (mypref-strlist word)))]
+         [(mypref? word) (mypref
+                          (+ (mypref-d word) 1)
+                          (mypref-t word)
+                          (update-inner (mypref-strlist word)))]
          [else word]))))
 
- (define (section t . etc)
-   (mypref
-       1
-       t
-       (for/list ([word (flatten etc)])
-         (cond
-           [(mypref? word) (mypref
-                            (+ (mypref-d word) 1)
-                            (mypref-t word)
-                            (update-inner (mypref-strlist word)))]
-           [else word]))))
-#|
- (define (section t . etc)
-   (mypref
-       1
-       t
-       words
-       (UpdateInner inner)
-|#
   (define (update-inner inner)
     (for/list ([word (flatten inner)])
       (cond
@@ -47,20 +43,10 @@
                          (update-inner (mypref-strlist word)))]
         [else word])))
 
-
-(define
-  (make-section d t . content)
-  (txexpr 'div '()
-      `(,(txexpr
-        (string->symbol (format "h~a" d))
-        '()
-        `(,t))
-      ,@content)))    
-    
-(provide section make-section update-inner show-inner)
-(provide
- (contract-out
-  [chapter (->* () #:rest (listof any/c) any)])))
+  (provide section update-inner show-inner)
+  (provide
+   (contract-out
+    [chapter (->* () #:rest (listof any/c) any)])))
 
 
 (require 'mymod)
